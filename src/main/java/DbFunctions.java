db496dd89323528fa1f625ed975a1f5bea3de433
Polymorph1024 <denisandreev93@gmail.com>
2023-05-09 02:25:08 +0300

    added basic CRUD functionality

--- src/main/java/DbFunctions.java
+++ src/main/java/DbFunctions.java
@@ -1,8 +1,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DbFunctions {
-    public Connection connectToDb(String dbName, String user, String password){
+    private Connection connection;
+
+    private Connection connectToDb(String dbName, String user, String password) {
         Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
@@ -17,6 +21,155 @@ public class DbFunctions {
             System.out.println(e);
         }
 
-        return  connection;
+        return connection;
+    }
+
+    public DbFunctions(String dbName, String user, String password) {
+        this.connection = connectToDb(dbName, user, password);
+    }
+
+    public void createTable(String tableName) {
+        Statement statement;
+        try {
+            String query = String.format("""
+                    CREATE TABLE %s (
+                    band_id SERIAL,
+                    name VARCHAR(64),
+                    label VARCHAR(64),
+                    genre VARCHAR(32),
+                    foundation_year SMALLINT,
+                    PRIMARY KEY(band_id));
+                    """, tableName);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Created");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void insertRow(String tableName, String name, String label, String genre, int foundationYear) {
+        Statement statement;
+        try {
+            String query = String.format("""
+                    INSERT INTO %s(name, label, genre, foundation_year)
+                    VALUES('%s', '%s', '%s', '%d');
+                    """, tableName, name, label, genre, foundationYear);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Row Inserted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void printHeader() {
+        System.out.printf("""
+                +----+--------------------------------+--------------------------------+----------------+----+
+                | id |              name              |              label             |     genre      |year|
+                +----+--------------------------------+--------------------------------+----------------+----+
+                """);
+    }
+
+    public void readData(String tableName) {
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            String query = String.format("""
+                    SELECT *
+                    FROM %s
+                    """, tableName);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            printHeader();
+            while (resultSet.next()) {
+                System.out.printf(" %-4s %-32s %-32s %-16s %d%n",
+                        resultSet.getString("band_id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("label"),
+                        resultSet.getString("genre"),
+                        resultSet.getInt("foundation_year"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void updateValue(String tableName, String name, String columnName, String newValue) {
+        Statement statement;
+        try {
+            String query = String.format("""
+                            UPDATE %s
+                            SET %s = '%s'
+                            WHERE name = '%s'
+                            """,
+                    tableName,
+                    columnName,
+                    newValue,
+                    name);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Updated");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void searchDataByValue(String tableName, String columnName, String value) {
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            String query = String.format("""
+                            SELECT *
+                            FROM %s
+                            WHERE %s = '%s'
+                            """,
+                    tableName,
+                    columnName,
+                    value);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            printHeader();
+            while (resultSet.next()) {
+                System.out.printf("%-4s %-32s %-32s %-16s %d\n",
+                        resultSet.getString("band_id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("label"),
+                        resultSet.getString("genre"),
+                        resultSet.getInt("foundation_year"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void deleteDataByValue(String tableName, String columnName, String value) {
+        Statement statement;
+        try {
+            String query = String.format("""
+                            DELETE FROM %s
+                            WHERE %s = '%s'
+                            """,
+                    tableName,
+                    columnName,
+                    value);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Deleted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void deleteTable(String tableName) {
+        Statement statement;
+        try {
+            String query = String.format("DROP TABLE %s", tableName);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Deleted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        DbFunctions dbFunctions = new DbFunctions();
-        dbFunctions.connectToDb("band_db", "postgres", "admin420");
+        DbFunctions db = new DbFunctions("band_db", "postgres", "admin420");
+//        db.createTable("band_info");
+//        db.insertRow("band_info", "Slayer", "Metal Blade Records", "trash metal", 1981);
+//        db.insertRow("band_info", "Burzum", "Deathlike Silence Productions", "black metal", 1991);
+//        db.insertRow("band_info", "Bob Marley and the Wailers", "Studio One", "reggae", 1963);
+//        db.insertRow("band_info", "Fall of Efrafa", "Fight for Your Mind", "crust punk", 2005);
+//        db.insertRow("band_info", "the ride of your life.", "DTH Studios", "surf punk", 2019);
+        db.readData("band_info");
     }
 }
